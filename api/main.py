4801d73dcd9295194697a27a17d6a9a9639472e3
Muhammad Nur <md.nurealamsiddiquee2004@gmail.com>
2025-01-30 17:05:57 +0600

    remove tomato prediction

--- api/main.py
+++ api/main.py
@@ -26,21 +26,21 @@ def read_root():
 
 
 potato_model = keras.models.load_model("./models/potato/4/model.keras")
-tomato_model = keras.models.load_model("./models/tomato/1/model.keras")
+# tomato_model = keras.models.load_model("./models/tomato/1/model.keras")
 
 potato_class = ["Early Blight", "Late Blight", "Healthy"]
-tomato_class = [
-    "Tomato_Bacterial_spot",
-    "Tomato_Early_blight",
-    "Tomato_Late_blight",
-    "Tomato_Leaf_Mold",
-    "Tomato Septoria leaf spot",
-    "Two spotted spider mite",
-    "Tomato__Target_Spot",
-    "Tomato__Tomato_YellowLeaf__Curl_Virus",
-    "Tomato__Tomato_mosaic_virus",
-    "Tomato_healthy",
-]
+# tomato_class = [
+#     "Tomato_Bacterial_spot",
+#     "Tomato_Early_blight",
+#     "Tomato_Late_blight",
+#     "Tomato_Leaf_Mold",
+#     "Tomato Septoria leaf spot",
+#     "Two spotted spider mite",
+#     "Tomato__Target_Spot",
+#     "Tomato__Tomato_YellowLeaf__Curl_Virus",
+#     "Tomato__Tomato_mosaic_virus",
+#     "Tomato_healthy",
+# ]
 
 
 def read_file_as_image(data) -> np.ndarray:
@@ -60,13 +60,13 @@ async def predict_potato(file: UploadFile = File(...)):
     return {"class": predicted_class, "confidence": f"{float(confidence*100)}%"}
 
 
-@app.post("/predict/tomato")
-async def predict_tomato(file: UploadFile = File(...)):
-    image = read_file_as_image(await file.read())
-    img_batch = np.expand_dims(image, 0)
+# @app.post("/predict/tomato")
+# async def predict_tomato(file: UploadFile = File(...)):
+#     image = read_file_as_image(await file.read())
+#     img_batch = np.expand_dims(image, 0)
 
-    predictions = tomato_model.predict(img_batch)
+#     predictions = tomato_model.predict(img_batch)
 
-    predicted_class = tomato_class[np.argmax(predictions[0])]
-    confidence = np.max(predictions[0])
-    return {"class": predicted_class, "confidence": f"{float(confidence*100)}%"}
+#     predicted_class = tomato_class[np.argmax(predictions[0])]
+#     confidence = np.max(predictions[0])
+#     return {"class": predicted_class, "confidence": f"{float(confidence*100)}%"}
